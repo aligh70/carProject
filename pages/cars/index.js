cd1b9da77dc31d1399f9ff284beec761e2dfe0ff
Ali Ghaffari <aligh71@yahoo.com>
2024-03-25 21:40:57 +0330

    add filter section with price and page related to it

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,12 @@
 import Categories from "@/components/module/Categories";
+import SearchBar from "@/components/module/SearchBar";
 import CarsPage from "@/components/templates/CarsPage";
 import carsData from "@/data/carsData";
+
 function AllCars() {
   return (
     <div>
+      <SearchBar />
       <Categories />
       <CarsPage data={carsData} />
     </div>
---
+++ pages/filter/[...slug].js
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+import carsData from "@/data/carsData";
+import CarsList from "@/components/templates/CarsList";
+
+function FilteredCars() {
+  const router = useRouter();
+  const [min, max] = router.query.slug || [];
+
+  const filteredData = carsData.filter(
+    (item) => item.price > min && item.price < max
+  );
+
+  if (!filteredData.length) return <h3>Not Found!</h3>;
+  return <CarsList data={filteredData} />;
+}
+
+export default FilteredCars;
