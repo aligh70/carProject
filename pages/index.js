c5f021a0cd42e40a4f05188bd8cc422e9760d2c8
Ali Ghaffari <aligh71@yahoo.com>
2024-03-25 21:56:35 +0330

    completion of the main page of the project

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-export default function Home() {
+import AllButton from "@/components/module/AllButton";
+import Categories from "@/components/module/Categories";
+import SearchBar from "@/components/module/SearchBar";
+import CarsPage from "@/components/templates/CarsPage";
+import carsData from "@/data/carsData";
+
+function Index() {
+  const cars = carsData.slice(0, 3);
   return (
     <div>
-      <h1>BotoCar project</h1>
+      <SearchBar />
+      <Categories />
+      <AllButton />
+      <CarsPage data={cars} />
     </div>
   );
 }
+
+export default Index;
